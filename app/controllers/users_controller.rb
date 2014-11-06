a508f56642b110aaa91da60916943ef3c3bed89a
MatsMyncke <mmyncke@gmail.com>
2014-11-06 20:07:18 +0100

    ik kom naar de kelder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :show]
+
+
   def new
   	@user = User.new
   end
@@ -18,10 +21,35 @@ class UsersController < ApplicationController
   	end
   end
 
+  def order
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :marks)
+    end
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 
 end
