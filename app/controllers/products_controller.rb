69dc9ce05a30b7698e6db0f79fa977fc8bd21dcf
ohsab <ohsab@github.com>
2014-11-25 13:09:55 +0100

    admin interface

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,12 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def index
+    @products = Product.all
+  end
+
   def edit
+    @product = Product.find(params[:id])
   end
 
   def delete
