bb234d74639cdc65ca332250b0f07b586cab45e8
Benjamin Cousaert <benj_cous@hotmail.com>
2014-12-09 17:28:24 +0100

    Fix routes for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    new_user_session_path
+    root_path
   end
 
   def after_sign_up_path_for(resource)
-    root_path
+    new_user_session_path
   end
 
     include OrdersHelper
