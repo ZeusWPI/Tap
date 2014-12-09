44a7b8630d485c8be4939c1ad06edef2ed6d4501
ohsab <ohsab@github.com>
2014-12-09 17:07:17 +0100

    application helper changed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,8 @@ module ApplicationHelper
     @color = Digest::MD5.hexdigest(user.name)[0..5]
   end
 
-  def get_inverted_color(user)
-    @inverted_color = Digest::MD5.hexdigest(user.name)[0..5].reverse
-  end
-
   def get_color_style(user)
-    @style = "background-color: #"+ get_color(user) +"; color: #"+
-    get_inverted_color(user)+ ";"
+    @style = "background-color: #"+ get_color(user) +";"
   end
 
   #tijdelijk voor layout
