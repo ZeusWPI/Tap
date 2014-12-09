db37c694aa873af37bdf480929a58ccb6d5e16e7
ohsab <ohsab@github.com>
2014-12-09 13:10:36 +0100

    color btn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,23 @@
+require 'digest/md5'
 module ApplicationHelper
+
+  def get_color(user)
+    @color = Digest::MD5.hexdigest(user.name)[0..5]
+  end
+
+  def get_inverted_color(user)
+    @inverted_color = Digest::MD5.hexdigest(user.name)[0..5].reverse
+  end
+
+  def get_color_style(user)
+    @style = "background-color: #"+ get_color(user) +"; color: #"+
+    get_inverted_color(user)+ ";"
+  end
+
   #tijdelijk voor layout
   def koelkast(status)
     @koelkast ||= status
   end
+
 
 end
