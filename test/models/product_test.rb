95987e48fefddd3539eb5780eb2fa86c75cf1c9a
benji <benj_cous@hotmail.com>
2015-03-03 17:41:53 +0100

    Add tests for orders and products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -18,7 +18,15 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "price behaves correctly" do
+    p = products(:fanta)
+
+    assert_equal p.price_cents, 60
+    assert_equal p.price, 0.6
+
+    p.price = 1.3
+
+    assert_equal p.price, 1.3
+    assert_equal p.price_cents, 130
+  end
 end
