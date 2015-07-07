849d9ce04b159c6832941c043bf552de5d1cf075
David Vandorpe <David.Vandorpe@UGent.be>
2015-07-07 20:09:58 +0200

    Product calories: DB migration + Model update + attribute permit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630183223) do
+ActiveRecord::Schema.define(version: 20150707162949) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer "order_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150630183223) do
     t.datetime "avatar_updated_at"
     t.integer  "category",            default: 0
     t.integer  "stock",               default: 0, null: false
+    t.integer  "calories"
   end
 
   create_table "users", force: :cascade do |t|
