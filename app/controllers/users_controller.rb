7b345f28a846b98c72fcd3fb6f982bc71933985f
benji <benj_cous@hotmail.com>
2015-09-01 17:40:18 +0200

    Do some styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,22 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
+  before_action :init, only: [:show, :edit, :update]
 
   def show
-    @user = User.find_by_id(params[:id]) || current_user
-    @orders = @user.orders
-      .order(:created_at)
-      .reverse_order
-      .paginate(page: params[:page])
-    @products = @user.products
-      .select("products.*", "sum(order_items.count) as count")
-      .group(:product_id)
-      .order("count")
-      .reverse_order
     @categories = @user.products
       .select("products.category", "sum(order_items.count) as count")
       .group(:category)
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Successfully updated!"
       redirect_to @user
     else
+      @user.reload
       render 'edit'
     end
   end
@@ -66,5 +56,18 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:avatar, :private)
+    end
+
+    def init
+      @user = User.find_by_id(params[:id]) || current_user
+      @orders = @user.orders
+        .order(:created_at)
+        .reverse_order
+        .paginate(page: params[:page])
+      @products = @user.products
+        .select("products.*", "sum(order_items.count) as count")
+        .group(:product_id)
+        .order("count")
+        .reverse_order
     end
 end
