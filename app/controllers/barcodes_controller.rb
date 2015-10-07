cf4cb740878030a8ee2350285ad2e72fdba75fe6
benji <benj_cous@hotmail.com>
2015-10-07 16:39:06 +0200

    authorization

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -1,6 +1,6 @@
 class BarcodesController < ApplicationController
-  load_and_authorize_resource :product
-  load_and_authorize_resource :barcode, through: :product, shallow: true
+  load_and_authorize_resource :product, only: :create
+  load_and_authorize_resource :barcode, through: :product, only: :create
 
   def create
     @barcode.save
