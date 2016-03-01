5f73efc7d4aa2b9c544ac00deccf1fcfb727725e
tl3ilaxu <kenneth0@live.be>
2016-03-01 17:15:29 +0100

    Don't let users generate money

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -13,7 +13,8 @@ class OrderItem < ActiveRecord::Base
   belongs_to :product
 
   validates :product, presence: true
-  validates :count,   presence: true, numericality: { only_integer: true }
+  validates :count,   presence: true, numericality: { only_integer: true,
+                                                      greater_than_or_equal_to: 0 }
 
   before_destroy :put_back_in_stock!
   after_create :remove_from_stock!
