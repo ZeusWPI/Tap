d9c7e5249ed36bcd7ef2518d573434771bc6a3e4
benji <benj_cous@hotmail.com>
2016-03-01 16:05:46 +0100

    Fix tests for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,14 +52,18 @@ class User < ActiveRecord::Base
 
   def balance
     @balance || begin
-      headers = {
-        "Authorization" => "Token token=#{Rails.application.secrets.tab_api_key}",
-        "Content-type" => "application/json"
-      }
-      result = HTTParty.get(File.join(Rails.application.config.api_url, "users", "#{name}.json"), headers: headers)
+      if Rails.env.test?
+        nil
+      else
+        headers = {
+          "Authorization" => "Token token=#{Rails.application.secrets.tab_api_key}",
+          "Content-type" => "application/json"
+        }
+        result = HTTParty.get(File.join(Rails.application.config.api_url, "users", "#{name}.json"), headers: headers)
 
-      if result.code == 200
-        JSON.parse(result.body)["balance"]
+        if result.code == 200
+          JSON.parse(result.body)["balance"]
+        end
       end
     rescue
     end
