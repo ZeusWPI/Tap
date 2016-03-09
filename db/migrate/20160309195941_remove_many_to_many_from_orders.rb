561a651545ac7b1f4e9104884fa6b7b0d777d997
benji <benj_cous@hotmail.com>
2016-03-09 21:41:14 +0100

    Oops delete orders we just duplicated

--- db/migrate/20160309195941_remove_many_to_many_from_orders.rb
+++ db/migrate/20160309195941_remove_many_to_many_from_orders.rb
@@ -3,5 +3,7 @@ class RemoveManyToManyFromOrders < ActiveRecord::Migration
     drop_table :order_items
 
     remove_column :orders, :price_cents, :integer
+
+    Order.where(product_id: nil).delete_all
   end
 end
