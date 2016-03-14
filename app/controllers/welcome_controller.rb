502f1ecef675412dcbc9cd93c200d1f8e9a09a78
benji <benj_cous@hotmail.com>
2016-03-14 20:30:16 +0100

    Welcome controller specs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,15 +2,14 @@ class WelcomeController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :token_sign_in
 
   def index
+    render json: { login_url: user_omniauth_authorize_url(:zeuswpi) }
   end
 
   def token_sign_in
     if user_signed_in? || params[:token] != Rails.application.secrets.koelkast_token
-      redirect_to root_path
-      return
+      return head :forbidden
     end
 
-    koelkast = User.koelkast
-    sign_in_and_redirect koelkast
+    sign_in_and_redirect User.koelkast
   end
 end
---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,39 @@
+describe WelcomeController, type: :controller do
+  include Devise::TestHelpers
+
+  #########
+  # INDEX #
+  #########
+
+  describe 'GET index' do
+    it 'should be successful' do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  #################
+  # TOKEN_SIGN_IN #
+  #################
+
+  describe 'GET token_sign_in' do
+    describe 'correct token' do
+      it 'should sign in a user' do
+        get :token_sign_in, token: "testtoken"
+        expect(warden.authenticated?(:user)).to be true
+      end
+    end
+
+    describe 'failed with' do
+      it 'already signed in' do
+        get :token_sign_in, token: "wrongtoken"
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'wrong token' do
+        get :token_sign_in, token: "wrongtoken"
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+end
