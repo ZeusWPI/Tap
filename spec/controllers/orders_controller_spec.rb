d3ff6767e1feb5e4d92c90fe3537182b21b26e72
benji <benj_cous@hotmail.com>
2016-03-14 20:37:29 +0100

    Add mockup for orders_controller tests, please fill in

---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,42 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  transaction_id :integer
+#  product_id     :integer
+#
+
+describe OrdersController, type: :controller do
+  before :each do
+    @user = create :user
+    sign_in @user
+  end
+
+  #######
+  # NEW #
+  #######
+
+  describe 'GET new' do
+
+  end
+
+  ##########
+  # CREATE #
+  ##########
+
+  describe 'POST create' do
+
+  end
+
+  ###########
+  # DESTROY #
+  ###########
+
+  describe 'DELETE destroy' do
+
+  end
+end
