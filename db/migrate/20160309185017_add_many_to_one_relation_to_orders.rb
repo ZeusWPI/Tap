dc1653bd162b7476b3a9c50484183030776fe8a1
benji <benj_cous@hotmail.com>
2016-06-22 17:50:43 +0200

    \o/

--- db/migrate/20160309185017_add_many_to_one_relation_to_orders.rb
+++ db/migrate/20160309185017_add_many_to_one_relation_to_orders.rb
@@ -4,14 +4,15 @@ class AddManyToOneRelationToOrders < ActiveRecord::Migration
 
     orders = Order.all
     orders.each do |order|
-      order.order_items.each do |oi|
+      OrderItems.where(order: order).each do |oi|
+        product = Product.where(id: oi.id).first
         oi.count.times do
           Order.create user_id: order.user_id,
-            price_cents: oi.product.price_cents,
+            price_cents: product.price_cents,
             created_at: order.created_at,
             updated_at: order.updated_at,
             transaction_id: order.transaction_id,
-            product: oi.product
+            product: product
         end
       end
     end
