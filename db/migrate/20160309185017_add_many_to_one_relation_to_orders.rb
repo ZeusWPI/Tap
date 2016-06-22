95aef6d9cc8ea6c528d0e7083878979d326e1365
benji <benj_cous@hotmail.com>
2016-06-22 18:02:18 +0200

    yesh this is gonna wurk

--- db/migrate/20160309185017_add_many_to_one_relation_to_orders.rb
+++ db/migrate/20160309185017_add_many_to_one_relation_to_orders.rb
@@ -2,21 +2,24 @@ class AddManyToOneRelationToOrders < ActiveRecord::Migration
   def up
     add_column :orders, :product_id, :integer
 
+    Order.skip_callback :create_api_job
+
     orders = Order.all
     orders.each do |order|
       OrderItem.where(order_id: order.id).each do |oi|
         product = Product.where(id: oi.id).first
         oi.count.times do
-          o = Order.new user_id: order.user_id,
+          Order.new user_id: order.user_id,
             price_cents: product.price_cents,
             created_at: order.created_at,
             updated_at: order.updated_at,
             transaction_id: order.transaction_id,
             product: product
-          o.save(validate: false)
         end
       end
     end
+
+    Order.reset_callbacks
   end
 
   def down
