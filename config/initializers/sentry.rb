477675a2ebd9f6340bdae810080432635f48f97d
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-12-16 00:07:29 +0100

    deps: Run `bundle update --all`.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.enable_tracing = true
+  config.enable_logs = true
   config.enabled_environments = %w[production]
 end
 
