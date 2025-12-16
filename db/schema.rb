e6c413419a5c25a2c1c4edb7894acc63c8fcb4c3
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-12-16 15:42:31 +0100

    deps: Complete the migration to rails 7.1.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_12_16_022119) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_16_022119) do
   create_table "barcodes", force: :cascade do |t|
     t.integer "product_id", null: false
     t.string "code", default: "", null: false
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_12_16_022119) do
     t.datetime "updated_at", precision: nil
     t.string "avatar_file_name"
     t.string "avatar_content_type"
-    t.integer "avatar_file_size"
+    t.bigint "avatar_file_size"
     t.datetime "avatar_updated_at", precision: nil
     t.integer "category", default: 0
     t.integer "stock", default: 0, null: false
@@ -74,7 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_12_16_022119) do
     t.integer "dagschotel_id"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
-    t.integer "avatar_file_size"
+    t.bigint "avatar_file_size"
     t.datetime "avatar_updated_at", precision: nil
     t.integer "orders_count", default: 0
     t.boolean "koelkast", default: false
