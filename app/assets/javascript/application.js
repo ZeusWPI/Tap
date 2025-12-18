1cfe7f4b67c1ab460290b93cdd22ecdd1c926148
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-12-19 00:46:36 +0100

    build(js): Compile our javascript utils with jsbundler.

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,28 @@
+// import "@hotwired/turbo-rails";
+// import "chartkick/chart.js";
+// import "vanilla-nested";
+// import PetiteVue from "petite-vue";
+// import Quagga from "@ericblade/quagga2";
+// import Rails from "@rails/ujs";
+
+// Start petite-vue on page and frame load
+// document.addEventListener("turbo:load", () => PetiteVue.createApp().mount());
+// document.addEventListener("turbo:frame-load", () => PetiteVue.createApp().mount());
+
+// Globally mount Quagga
+// window.Quagga = Quagga;
+
+// Rails.start();
+
+// Register a service worker
+import "./pwa";
+
+// Globally mount some utils
+import "./utils/filepicker";
+import "./utils/modal";
+import "./utils/remove";
+import "./utils/scanner";
+import "./utils/submit";
+
+// Script that only runs for koelkast user
+//import "./mqtt.js"
