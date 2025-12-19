77b1232036f8f2604ebf2b1f463c12ee784fac6e
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-12-19 01:40:12 +0100

    deps: Add back all needed javascript libraries.

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,18 +1,18 @@
-// import "@hotwired/turbo-rails";
-// import "chartkick/chart.js";
-// import "vanilla-nested";
-// import PetiteVue from "petite-vue";
-// import Quagga from "@ericblade/quagga2";
-// import Rails from "@rails/ujs";
+import "@hotwired/turbo-rails";
+import "chartkick/chart.js";
+import "vanilla-nested";
+import * as PetiteVue from "petite-vue";
+import Quagga from "@ericblade/quagga2";
+import Rails from "@rails/ujs";
 
 // Start petite-vue on page and frame load
-// document.addEventListener("turbo:load", () => PetiteVue.createApp().mount());
-// document.addEventListener("turbo:frame-load", () => PetiteVue.createApp().mount());
+document.addEventListener("turbo:load", () => PetiteVue.createApp().mount());
+document.addEventListener("turbo:frame-load", () => PetiteVue.createApp().mount());
 
 // Globally mount Quagga
-// window.Quagga = Quagga;
+window.Quagga = Quagga;
 
-// Rails.start();
+Rails.start();
 
 // Register a service worker
 import "./pwa";
@@ -25,4 +25,4 @@ import "./utils/scanner";
 import "./utils/submit";
 
 // Script that only runs for koelkast user
-//import "./mqtt.js"
+import "./mqtt"
