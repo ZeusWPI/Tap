10b0015eb99e6f1379bf0d4abcb4188a1026b0db
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-12-21 03:41:01 +0100

    deploy: Rename omniauth/oauth config vars to zauth.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,8 +275,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :zeuswpi,
-                  Rails.application.secrets.omniauth_client_id,
-                  Rails.application.secrets.omniauth_client_secret,
+                  Rails.application.secrets.zauth_client_id,
+                  Rails.application.secrets.zauth_client_secret,
                   authorize_params: {scope: 'roles'}
 
   # ==> Warden configuration
